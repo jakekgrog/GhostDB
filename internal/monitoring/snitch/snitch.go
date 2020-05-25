061bf314c790e5aa29302759903145958fa2d146
jakekgrog <jake.kgrogan@gmail.com>
2020-05-25 14:12:27 +0100

    Added snitch log rotate

--- internal/monitoring/snitch/snitch.go
+++ internal/monitoring/snitch/snitch.go
@@ -3,6 +3,8 @@ package snitch
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/user"
@@ -12,6 +14,8 @@ import (
 
 const (
 	SnitchLogFileName = "/ghostdb/ghostdb_snitch.log"
+	SnitchTempFileName = "/ghostdb/ghostdb_snitch_tmp.log"
+	MaxSnitchLogSize = 20
 )
 
 type Snitch struct {
@@ -189,6 +193,18 @@ func StartSnitchMonitor() {
 	usr, _ := user.Current()
 	configPath := usr.HomeDir
 
+	needRotate, err := logMustRotate(configPath + SnitchLogFileName)
+	if err != nil {
+		log.Fatalf("failed to check if log needs to be rotated: %s", err.Error())
+	}
+	if needRotate {
+		nBytes, err := Rotate()
+		if err != nil {
+			log.Fatalf("failed to rotate snitch log file: %s", err.Error())
+		}
+		log.Printf("successfully rotated snitch log file: %d bytes rotated", nBytes)
+	}
+
 	file, err := os.OpenFile(configPath + SnitchLogFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("failed to open snitch log file: %s", err.Error())
@@ -228,4 +244,104 @@ func GetSnitchMetrics() []Snitch {
 		data = append(data, entry)
 	}
 	return data
+}
+
+func logMustRotate(logfile string) (bool, error) {
+	fi, err := os.Stat(logfile)
+	if err != nil {
+		return false, err
+	}
+	// get the size
+	size := fi.Size()
+	if size >= MaxSnitchLogSize {
+		return true, nil
+	}
+	return false, nil
+}
+
+// Rotate rotates the main snitch log by copying the contents of the 
+// log file to a temporary log and clearing the main log.
+// If there is data in the temp log file, it is cleared. 
+func Rotate() (int64, error) {
+	usr, _ := user.Current()
+	configPath := usr.HomeDir
+
+	src := configPath + SnitchLogFileName
+	tmp := configPath + SnitchTempFileName
+
+	// Check if tmp file exists
+	exists, err := tmpFileExists(tmp)
+	if err != nil {
+		return 0, fmt.Errorf("Error when checking for temp log existence: %s", err.Error())
+	}
+
+	// If it exists, clear it
+	if exists {
+		_, err := cleanFile(tmp)
+		if err != nil {
+			return 0, fmt.Errorf("failed to clean temp log")
+		}
+	}
+
+	// Open the source file (main log)
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, fmt.Errorf("failed to stat log file")
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	// Open the tmp log (or create if it doesn't exist)
+	dst, err := os.OpenFile(tmp, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return 0, fmt.Errorf("failed to open %s temporary log file", tmp)
+	}
+	defer dst.Close()
+
+	// Copy the contents of main log to tmp log
+	nBytes, err := io.Copy(dst, source)
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to copy log to temp log")
+	}
+
+	// clear the main log
+	_, err = cleanFile(src)
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to clean snitch log file")
+	}
+
+	return nBytes, err
+}
+
+func tmpFileExists(tmpFilename string) (bool, error) {
+	if _, err := os.Stat(tmpFilename); os.IsNotExist(err) {
+		return false, err
+	}
+	return true, nil
+}
+
+func cleanFile(filePath string) (bool, error) {
+	err := os.Remove(filePath)
+
+	if err != nil {
+		return false, err
+	}
+
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	return true, err
 }
