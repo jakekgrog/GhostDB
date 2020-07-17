ad094d5317d42f37951088fa917d7707fe957c86
lihkak <kerhalkar.akhil@gmail.com>
2020-07-17 08:11:21 +0530

    changed conf input to yml

--- config/config_reader.go
+++ config/config_reader.go
@@ -31,12 +31,14 @@
 package config
 
 import (
-	"encoding/json"
 	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 const (
-	ConfigFileLinux = "/etc/ghostdb/ghostdbConf.json"
+	ConfigFileLinux = "/etc/ghostdb/ghostdbConf.yml"
 
 	DefaultKeyspaceSize      = 65536
 	DefaultSysMetricInterval = 300  // 5 minutes
@@ -55,57 +57,57 @@ const (
 type Configuration struct {
 	// KeyspaceSize represents the maximum number
 	// of key-value pairs allowed in the cache
-	KeyspaceSize int32
+	KeyspaceSize int32 `yaml:"keyspaceSize"`
 
 	// SysMetricInterval represents the interval
 	// in seconds of when the sys metrics logging subsystem
 	// should output to the log file.
-	SysMetricInterval int32
+	SysMetricInterval int32 `yaml:"sysMetricInterval"`
 
 	// AppMetricInterval represents the interval
 	// in seconds of when the app metrics logging subsystem
 	// should output to the log file.
-	AppMetricInterval int32
+	AppMetricInterval int32 `yaml:"appMetricInterval"`
 
 	// DefaultTTL represents the default time-to-live
 	// for key-value pairs in the cache. If set to -1
 	// the key-value pair will never expire
-	DefaultTTL int32
+	DefaultTTL int32 `yaml:"defaultTTL"`
 
 	// CrawlerInterval is the time, in seconds, of how
 	// often the crawlers should crawl the cache to remove
 	// stale key-value pairs.
-	CrawlerInterval int32
+	CrawlerInterval int32 `yaml:"crawlerInterval"`
 
 	// SnapshotInterval is the time, in seconds, of how
 	// often the snapshot scheduler should create a
 	// point-in-time snapshot of the cache.
-	SnapshotInterval int32
+	SnapshotInterval int32 `yaml:"snapshotInterval"`
 
 	// Snapshot enabled is a bool which enables snapshotting.
 	// It takes precedence over AOF persistence.
-	SnapshotEnabled bool
+	SnapshotEnabled bool `yaml:"snapshotEnabled"`
 
 	// PersistenceAOF is a bool which enables AOF persistence.
 	// Snapshots take precedence if both are enabled.
-	PersistenceAOF bool
+	PersistenceAOF bool `yaml:"persistenceAOF"`
 
 	// AOFMaxBytes represents the maximum number of bytes to be
 	// written to the append-only-file log before an AOF log
 	// rotation takes place.
-	AofMaxBytes int64
+	AofMaxBytes int64 `yaml:"aofMaxByteSize"`
 
 	// EntryTimestamp is a boolean which enables timestamping for
 	// entries in the appMetrics logging subsystem.
-	EntryTimestamp bool
+	EntryTimestamp bool `yaml:"entryTimestamp"`
 
 	// EnableEncryption is a bool that enables encryption of
 	// snapshots using 128-bit AES.
-	EnableEncryption bool
+	EnableEncryption bool `yaml:"enableEncryption"`
 
 	// Passphrase is the passphrase to be used for snapshot encryption
 	// should it be enabled.
-	Passphrase string
+	Passphrase string `yaml:"passphrase"`
 }
 
 // InitializeConfiguration initializes the cache configuration object
@@ -139,15 +141,16 @@ func (conf *Configuration) SetDefaultParams() {
 func InitializeFromConfig() (Configuration, error) {
 	var config Configuration
 
-	file, err := ioutil.ReadFile(ConfigFileLinux)
+	file, err := os.Open(ConfigFileLinux)
+	defer file.Close()
 	if err != nil {
 		return Configuration{}, err
 	}
 
-	err = json.Unmarshal(file, &config)
+	bytes, _ := ioutil.ReadAll(file)
+	err = yaml.Unmarshal(bytes, &config)
 	if err != nil {
 		return Configuration{}, err
 	}
-
 	return config, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ghostdb/ghostdb-cache-node
 
 go 1.13
 
-require github.com/valyala/fasthttp v1.12.0
+require (
+	github.com/valyala/fasthttp v1.12.0
+	gopkg.in/yaml.v2 v2.3.0
+)
